2c99edd5033db45ac93e428bb379524e357ef393
DESKTOP-33RVBQE\Yohan <dygks910910@daum.net>
2018-06-25 21:21:41 +0900

    충돌문제 raycast로 해결.shapeDestroy 이팩트 추가. 리소스 변경.파티클 prefab추가.

---
+++ BBTan/Assets/CollisionShapeInteraction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionShapeInteraction : MonoBehaviour {
+    public short hp;
+
+    float hpWeight;
+    private SpriteRenderer spRenderer;
+	// Use this for initialization
+	void Start () {
+        spRenderer = GetComponent<SpriteRenderer>();
+        spRenderer.color = Color.red;
+        hpWeight = 0.92f / hp;
+	}
+    public void Hit()
+    {
+        hp -= 1;
+        if(hp <= 0)
+        {
+            DestroyEffect();
+            Destroy(gameObject);
+        }
+        else
+        {
+            print(spRenderer.color);
+            spRenderer.color += new Color(0.0f, hpWeight, 0,0);
+        }
+    }
+    private void DestroyEffect()
+    {
+
+        GameObject prefab = Resources.Load("Prefabs/DestroyShapeEffect") as GameObject;
+        prefab.gameObject.transform.position = transform.position;
+
+        GameObject particle = MonoBehaviour.Instantiate(prefab) as GameObject;
+        particle.name = "ParticleEffect";
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- BBTan/Assets/Scripts/move.cs
+++ BBTan/Assets/Scripts/move.cs
@@ -1,34 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class move : MonoBehaviour {
     public float speed;
     public Vector3 dir;
-    private Rigidbody2D rigidbd;
     private Vector3 pos;
+    private Vector3 lastPosition;
+
+    private LayerMask collisionLayer;
 	// Use this for initialization
 	void Start () {
-        pos = transform.position;
-        rigidbd = GetComponent<Rigidbody2D>();
+        collisionLayer = LayerMask.GetMask("RayColliderObj");
     }
-
-    
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        print("collision");
-        Vector3 n = collision.contacts[0].normal;
-        dir = dir + (2 * n * (Vector3.Dot(-dir, n)));
-        dir.Normalize();
-    }
-   
     // Update is called once per frame
     void Update () {
-        
-	}
+      
+    }
     private void FixedUpdate()
     {
+
+        Vector3 translateVector = (this.dir * speed) * Time.deltaTime;
+        transform.position += translateVector;
+        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, Vector3.Magnitude(translateVector), collisionLayer);
+        if (hit)
+        {
+            
+            CollisionShapeInteraction tmp = hit.collider.GetComponent<CollisionShapeInteraction>();
+            if(tmp)
+            {
+                tmp.Hit();
+            }
+            Vector3 n = hit.normal;
+            //반사벡터.
+            dir = dir + (2 * n * (Vector2.Dot(-dir, n)));
+            dir.Normalize();
+        }
+
+        Debug.DrawRay(transform.position, dir * speed * Time.deltaTime, Color.red);
         //transform.Translate(dir * speed);
-        rigidbd.MovePosition(transform.position + (dir * speed));
+        //rigidbd.MovePosition(transform.position + (dir * speed));
+
     }
 }
